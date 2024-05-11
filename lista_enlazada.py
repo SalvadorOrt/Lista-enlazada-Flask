370a347cb60d85670a30bfcbc6de730aca342a4d
SalvadorOrt <salvadorortega505@gmail.com>
2024-05-11 18:13:31 -0500

    Primer commit

---
+++ lista_enlazada.py
@@ -0,0 +1,41 @@
+class Nodo:
+    def __init__(self,dato):
+        self.elemento = dato
+        self.next = None
+class Lista:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+        self.length = 0
+    def print(self):
+        temp = self.head
+        cadena = ''
+        while(temp is not None):
+            print(temp.elemento)
+            cadena += str(temp.elemento) + ' '
+            temp = temp.next
+        return cadena
+    def append(self,dato):
+        nodo = Nodo(dato)
+        if self.length == 0:
+            self.head = nodo
+            self.tail = nodo
+        else:
+            self.tail.next = nodo
+            self.tail = nodo
+        self.length += 1
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head 
+        pre = self.head
+        while(temp.next is not None):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return str(temp.elemento)
---
+++ web.py
@@ -0,0 +1,33 @@
+from flask import Flask,request,render_template_string
+from lista_enlazada import Lista
+app = Flask(__name__)
+
+lista = Lista()
+@app.route('/',methods = ['GET','POST'])
+def root():
+    global lista
+    var = ''
+    if request.method == 'POST':
+        if 'agregar' in request.form:
+            num = request.form["valor"]
+            print(num)
+            lista.append(num)
+            print(lista.print())
+        elif 'eliminar' in request.form:
+            var = lista.pop()
+    cadena = '''
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+            <form method = 'post'>
+                <input name = "valor">
+                <button name = "agregar" type="submit">Agregar</button>
+            </form>
+            <form method = 'post'>
+                <button name = "eliminar" type="submit">Eliminar</button>
+            </form>
+            <h3>Elementos de la lista: {{lista}}</h3>
+            <h3>Elementos eliminad: {{eliminado}}</h3>
+            '''
+    return render_template_string(cadena,lista = lista.print(),eliminado = var)
+
+if __name__ == '__main__':
+    app.run(debug=True)
